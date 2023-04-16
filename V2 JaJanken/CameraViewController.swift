d809b91565a43a8432f2279627de2772e60bee14
Carlos Mbendera <carlosmbendera@gmail.com>
2023-04-15 20:09:54 -0500

    Land Mark

--- V2 JaJanken/CameraViewController.swift
+++ V2 JaJanken/CameraViewController.swift
@@ -9,43 +9,50 @@ import AVFoundation
 import UIKit
 import Vision
 
-enum errors: Error{
-    //TODO: Write actual error cases
-    case TooLazyToWrite
+enum errors: Error {
+    case cameraError
 }
 
-final class CameraViewController : UIViewController{
+final class CameraViewController: UIViewController {
     
+    private var lastBeepTime: Date?
+
     private var cameraFeedSession: AVCaptureSession?
+    
+    private var coreMLRequest: VNCoreMLRequest!
     
     override func loadView() {
         view = CameraPreview()
     }
     
-    private var cameraView: CameraPreview{ view as! CameraPreview}
+    private var cameraView: CameraPreview { view as! CameraPreview }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard let coreMLModel = try? VNCoreMLModel(for: WalletObject().model) else {
+            fatalError("Failed to load the Core ML model")
+        }
+        
+        coreMLRequest = VNCoreMLRequest(model: coreMLModel) { [weak self] request, error in
+            self?.handleDetectionResults(request: request, error: error)
+        }
+        coreMLRequest.imageCropAndScaleOption = .scaleFill
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        do{
-            
-            if cameraFeedSession == nil{
+        do {
+            if cameraFeedSession == nil {
                 try setupAVSession()
-                
                 cameraView.previewLayer.session = cameraFeedSession
-                //MARK: Commented out cause it cropped
-             //   cameraView.previewLayer.videoGravity = .resizeAspectFill
             }
             
-            //MARK: Surronded the code into a DispatchQueue Cause we were having crashes
             DispatchQueue.global(qos: .userInteractive).async {
                 self.cameraFeedSession?.startRunning()
-               }
+            }
             
-        }catch{
+        } catch {
             print(error.localizedDescription)
         }
     }
@@ -58,53 +65,72 @@ final class CameraViewController : UIViewController{
     private let videoDataOutputQueue =
         DispatchQueue(label: "CameraFeedOutput", qos: .userInteractive)
     
-    
     func setupAVSession() throws {
-        //Start of setup
         guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
-            throw errors.TooLazyToWrite
+            throw errors.cameraError
         }
         
-        guard let deviceInput = try? AVCaptureDeviceInput(device: videoDevice) else{
-            throw errors.TooLazyToWrite
+        guard let deviceInput = try? AVCaptureDeviceInput(device: videoDevice) else {
+            throw errors.cameraError
         }
         
         let session = AVCaptureSession()
         session.beginConfiguration()
         session.sessionPreset = AVCaptureSession.Preset.high
         
-        guard session.canAddInput(deviceInput) else{
-            throw errors.TooLazyToWrite
+        guard session.canAddInput(deviceInput) else {
+            throw errors.cameraError
         }
         
         session.addInput(deviceInput)
         
         let dataOutput = AVCaptureVideoDataOutput()
-        if session.canAddOutput(dataOutput){
+        if session.canAddOutput(dataOutput) {
             session.addOutput(dataOutput)
             dataOutput.alwaysDiscardsLateVideoFrames = true
             dataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
-        }else{
-            throw errors.TooLazyToWrite
+        } else {
+            throw errors.cameraError
         }
         
         session.commitConfiguration()
         cameraFeedSession = session
     }
     
-    
-
-        
-    }
-
-
-    extension CameraViewController: AVCaptureVideoDataOutputSampleBufferDelegate{
-        //Handler and Observation
-        
-        func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+    private func handleDetectionResults(request: VNRequest, error: Error?) {
+        DispatchQueue.main.async {
+            guard let results = request.results as? [VNRecognizedObjectObservation] else {
+                return
+            }
             
-           
-            
+            let currentTime = Date()
+             let minimumTimeBetweenBeeps: TimeInterval = 2
+             
+             
+            if !results.isEmpty{
+              //   AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+                AudioServicesPlaySystemSound(1103)
+                print(results)
+                 self.lastBeepTime = currentTime
+             }
+            // Process the results (e.g., filter objects by class and confidence, and notify the SwiftUI view)
         }
-        
     }
+
+}
+
+
+
+extension CameraViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .up, options: [:])
+        do {
+            try handler.perform([coreMLRequest])
+        } catch {
+            print("Failed to perform the object detection request: \(error)")
+        }
+    }
+}
